0b4f72636c311f0b8802b8cadca2a94bf3267381
Sajethan Satkunarajah <sajetku@gmail.com>
2023-05-24 16:33:26 +0100

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "07828753143"}
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(name: "Chicago Spaghetti", address: "7 Boundary St, London E2 7JE", phone_number: "27613862371", category: "italian")
+Restaurant.create!(name: "Pasta Remoli", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "62316372823", category: "italian")
+Restaurant.create!(name: "Wagamama", address: "406 High St, London E3 6SQ", phone_number: "37153672617", category: "japanese")
+Restaurant.create!(name: "McDonald's", address: "608 Borrodaile Rd, London E2 9PQ", phone_number: "647625347636", category: "french")
+Restaurant.create!(name: "Nando's", address: "900 Drayton Rd, London UB1 3JA", phone_number: "83782739816", category: "belgian" )
+puts "Finished!"
